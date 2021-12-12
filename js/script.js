b3a0bebb1e59675c8c7d1dbd33873b63397691a5
Venus-Kavunza <venusmwende@gmail.com>
2021-12-12 22:07:25 +0300

    Add more changes to Function for the form

--- js/script.js
+++ js/script.js
@@ -4,25 +4,25 @@ function AkanGenerate(e, day, month, year) {
     month = parseInt(document.getElementById("month").value);
     year = parseInt(document.getElementById("year").value);
 
-    if ((day < 1 ) || (day > 31) || isNaN(day)) {
+    if ((day < 1) || (day > 31) || isNaN(day)) {
         $(document.getElementById("invalid")).show();
-        $(document.getElementById("output")).hide(); 
+        $(document.getElementById("output")).hide();
         document.getElementById("invalid").innerHTML = "Day is invalid. Please enter a valid Day";
     } else if ((month < 1) || (month > 12) || isNaN(month)) {
         $(document.getElementById("invalid")).show();
-        $(document.getElementById("output")).hide(); 
+        $(document.getElementById("output")).hide();
         document.getElementById("invalid").innerHTML = "Month is invalid. Please enter a valid Month";
-    } else if ((year< 1200) || isNaN(year)) {
+    } else if ((year < 1200) || isNaN(year)) {
         $(document.getElementById("invalid")).show();
-        $(document.getElementById("output")).hide(); 
+        $(document.getElementById("output")).hide();
         document.getElementById("invalid").innerHTML = "Year is invalid. Please enter a valid Year";
     } else if ((document.getElementById("male").checked == false) && (document.getElementById("female").checked == false)) {
         $(document.getElementById("invalid")).show();
-        $(document.getElementById("output")).hide(); 
+        $(document.getElementById("output")).hide();
         document.getElementById("invalid").innerHTML = "Please select your Gender";
     } else {
-        document.getElementById("invalid").hide();
-   
+        $(document.getElementById("invalid")).hide();
+
         var wkday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         var ml = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
         var fml = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
@@ -38,11 +38,11 @@ function AkanGenerate(e, day, month, year) {
             $(document.getElementById("output")).show();
             document.getElementById("output").innerHTML =("You were born on " + dayOfWeek + ".Your name is " + nm + ".")
 
-     }
+        }
+
     }
 }
-
-
+/*Reset Button */
 function resetForm() {
     $(document.getElementById("invalid")).hide();
     $(document.getElementById("output")).hide();
